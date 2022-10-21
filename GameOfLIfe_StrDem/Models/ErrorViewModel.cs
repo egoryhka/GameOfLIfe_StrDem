2fddddee82d13e529cef55a97e596fb4cc79ba1c
egoryhka <egor.demyanenko2000@mail.ru>
2022-10-21 12:05:45 +0300

    Добавьте файлы проекта.

---
+++ GameOfLIfe_StrDem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameOfLIfe_StrDem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
